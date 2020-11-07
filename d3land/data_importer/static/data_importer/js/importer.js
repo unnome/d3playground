26dafa1e8cd725bbb5cc2a4dbf40c559b7cf7164
Ivan Cazzamali <ivan.cazzamali@protonmail.com>
2020-11-08 00:42:13 +0100

    added mouseover event, as well as some fancier x and y coordinates. also added the dataset

--- d3land/data_importer/static/data_importer/js/importer.js
+++ d3land/data_importer/static/data_importer/js/importer.js
@@ -4,35 +4,35 @@ const objCsv = {
   columns: [],
 };
 
-function makeTable(input) {
-  // from the objCsv.dataFile information, we
-  // represent it as a table in html in the data_table
-  // div.
-  // we guess the table theads from the objCsv.columns information.
-  const target = document.querySelector('#data_table');
-  const table = document.createElement('table');
-  // make table theads
-  objCsv.columns.forEach(col => {
-    thead = document.createElement('th');
-    text = document.createTextNode(col);
-    thead.appendChild(text);
-    table.appendChild(thead);
-  });
-  // populate table
-  objCsv.dataFile.forEach(line => {         // for each line in the data...
-    tr = document.createElement('tr');     // ... create a line in the table.
-    elementsInLine = line.split(',');
-    elementsInLine.forEach(element => {               // for each element in the row...
-      td = document.createElement('td');   // ... create a cell in that row
-      text = document.createTextNode(element);
-      td.appendChild(text);
-      tr.appendChild(td);
-    });
-    table.appendChild(tr);
-  });
-
-  target.appendChild(table);
-}
+// function makeTable(input) {
+//   // from the objCsv.dataFile information, we
+//   // represent it as a table in html in the data_table
+//   // div.
+//   // we guess the table theads from the objCsv.columns information.
+//   const target = document.querySelector('#data_table');
+//   const table = document.createElement('table');
+//   // make table theads
+//   objCsv.columns.forEach(col => {
+//     thead = document.createElement('th');
+//     text = document.createTextNode(col);
+//     thead.appendChild(text);
+//     table.appendChild(thead);
+//   });
+//   // populate table
+//   objCsv.dataFile.forEach(line => {         // for each line in the data...
+//     tr = document.createElement('tr');     // ... create a line in the table.
+//     elementsInLine = line.split(',');
+//     elementsInLine.forEach(element => {               // for each element in the row...
+//       td = document.createElement('td');   // ... create a cell in that row
+//       text = document.createTextNode(element);
+//       td.appendChild(text);
+//       tr.appendChild(td);
+//     });
+//     table.appendChild(tr);
+//   });
+// 
+//   target.appendChild(table);
+// }
 
 function parseData(input) {
   const csvData = [];
@@ -47,7 +47,7 @@ function parseData(input) {
   lineBreak.forEach((res) => {
     csvData.push(res.split(','));
   });
-  makeTable(csvData);
+  //makeTable(csvData);
 }
 
 function uploadFile(input) {
@@ -82,6 +82,14 @@ function plotIt(input) {
   var width = 800;
   var height = 600;
 
+  var xScale = d3.scaleLinear()
+    .domain([0, 7])
+    .range([0, width]);
+
+  var yScale = d3.scaleLinear()
+    .domain([0, 4])
+    .range([0, height]);
+
   var shape = d3.arc({
     innerRadius: 0,
     outerRadius: 8,
@@ -101,8 +109,24 @@ function plotIt(input) {
     .data(input)
     .enter()
     .append("circle")
-    .attr("cx", function() {return Math.random() * width;})
-    .attr("cy", function() {return Math.random() * height;})
+    .attr("fill", "black")
+    .attr("stroke", "white")
+    .attr("opacity", 0.5)
+    .attr("id", function(d) {
+      row = d.split(',');
+      type = row[0];
+      return type;
+    })
+    .attr("cx", function(d) {
+      row = d.split(',');
+      value = row[3];
+      return xScale(value) ;
+    })
+    .attr("cy", function(d) {
+      row = d.split(',');
+      value = row[4];
+      return yScale(value) ;
+    })
     .attr("r", function(d) {
       row = d.split(',');
       type = row[2];
@@ -127,5 +151,17 @@ function plotIt(input) {
         return  "black";
       }
     })
-    ;
+    .on("mouseenter", function () {
+      d3.select(this)
+        .transition()
+        .duration(500)
+        .attr("r", 50);
+    })
+
+    .on("mouseleave", function () {
+      d3.select(this)
+        .transition()
+        .duration(500)
+        .attr("r",objCsv.dataFile[this.id].split(',')[2]);
+    });
 }
